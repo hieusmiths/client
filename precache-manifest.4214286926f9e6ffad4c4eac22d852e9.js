6664a66cc93076024a4e0410c6e3aedc130e2f30
hieusmiths <hieusmiths@gmail.com>
2019-03-27 23:49:55 +0700

    Updates

---
+++ precache-manifest.4214286926f9e6ffad4c4eac22d852e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f1a12bb5d1cdf91f65",
+    "url": "/client/static/js/runtime~main.3fca2621.js"
+  },
+  {
+    "revision": "18d2b5ac3aeb74074837",
+    "url": "/client/static/js/main.e228c3df.chunk.js"
+  },
+  {
+    "revision": "4219342b345285bf3428",
+    "url": "/client/static/js/2.7c5dc20e.chunk.js"
+  },
+  {
+    "revision": "18d2b5ac3aeb74074837",
+    "url": "/client/static/css/main.9bed2d61.chunk.css"
+  },
+  {
+    "revision": "1e9a61a1b3fd983d0d39ae07e2855902",
+    "url": "/client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/client/precache-manifest.1ecf8243300d52ff31d6b7f171a3e951.js"
+  "/client/precache-manifest.4214286926f9e6ffad4c4eac22d852e9.js"
 );
 
 workbox.clientsClaim();
