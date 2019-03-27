19f0281ac81b6d65caecf50b7598fa2d57dae08b
hieusmiths <hieusmiths@gmail.com>
2019-03-27 23:53:20 +0700

    ok ss

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       displayForm : false,
-      dataUser : [],
+      dataUser : DataUser,
       textForSearch : '',
       displayFormEdit : false,
     }
